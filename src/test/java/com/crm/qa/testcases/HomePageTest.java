7200f1713c97ae80e0bd79e60c09b0254d5fe9bb
Ramkumar Reddy Boddu <Ramkumar.Reddy.Boddu@ngahr.com>
2019-01-17 21:52:55 +0530

    Free CRM App Test

---
+++ src/test/java/com/crm/qa/testcases/HomePageTest.java
@@ -0,0 +1,71 @@
+package com.crm.qa.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.crm.qa.base.TestBase;
+import com.crm.qa.pages.ContactsPage;
+import com.crm.qa.pages.HomePage;
+import com.crm.qa.pages.LoginPage;
+import com.crm.qa.util.TestUtil;
+
+
+
+public class HomePageTest extends TestBase {
+	LoginPage loginPage;
+	HomePage homePage;
+	ContactsPage contactsPage;
+	TestUtil testUtil;
+	
+	
+	public HomePageTest() {
+		super();
+	}
+	
+	//test cases should be separated -- independent with each other
+	//before each test case -- launch the browser and login
+	//@test -- execute test case
+	//after each test case -- close the browser
+	
+	 
+	@BeforeMethod
+	public void setUp() throws InterruptedException {
+		initialization();
+		testUtil = new TestUtil();
+		contactsPage = new ContactsPage();
+		loginPage = new LoginPage();
+		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+	}
+	
+	@Test(priority=1)
+	public void verifyHomePageTitleTest()  {
+		String homePageTitle = homePage.verifyHomePageTitle();
+		//Assert.assertEquals(homePageTitle, "CRMPRO");
+		Assert.assertEquals(homePageTitle, "CRMPRO", "Home Page litle is not matched");
+	}
+	
+	@Test(priority=2)
+	public void verifyCorrectUserNameTest() {
+		testUtil.switchToFrame();
+		Assert.assertTrue(homePage.verifyCorrectUserName());
+	}
+	
+	@Test(priority=3)
+	public void verifyContactsLinkTest() {
+		testUtil.switchToFrame();
+		contactsPage = homePage.clickOnContactsLink();
+		
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	
+	
+	
+
+}
